65cc777db687e1af1ab8198cb767973d5f1c34ca
Guillaume Clement <guillaumecle.me@gmail.com>
2023-11-22 21:00:47 -0700

    Update logger reference

--- src/runtime/commit.ts
+++ src/runtime/commit.ts
@@ -5,18 +5,18 @@ import { ConsolaInstance } from 'consola'
 
 export function commitHook(options: ModuleOptions, logger: ConsolaInstance){
     try {
-        executeCommand('git version');
+        executeCommand('git version', logger);
     } catch (error) {
         throw new Error(`Could not find 'git', is it installed? Error: ${error}`);
     }
 
     const commitOptions = typeof options.commitHook == "object" ? options.commitHook : {};
 
-    executeCommand('git add .')
-    executeCommand('git config user.name "' + commitOptions.commitAuthorName ?? 'Nuxt GPT Content' + '"')
-    executeCommand('git config user.email "' + commitOptions.commitAuthorEmail ?? 'no-reply@nuxt.com' + '"')
-    executeCommand('git commit -m "' + commitOptions.commitMessage ?? 'Adding content changes' + '"')
-    executeCommand('git push')
+    executeCommand('git add .', logger)
+    executeCommand('git config user.name "' + commitOptions.commitAuthorName ?? 'Nuxt GPT Content' + '"', logger)
+    executeCommand('git config user.email "' + commitOptions.commitAuthorEmail ?? 'no-reply@nuxt.com' + '"', logger)
+    executeCommand('git commit -m "' + commitOptions.commitMessage ?? 'Adding content changes' + '"', logger)
+    executeCommand('git push', logger)
 }
 
 function executeCommand(command: string, logger: ConsolaInstance){
